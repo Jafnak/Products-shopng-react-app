c675e7481a3c02653a128638f298cd074976b0be
Jafnak <jafnakusman@gmail.com>
2024-06-08 20:16:37 +0530

    View  component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Add from './components/Add';
 import Search from './components/Search';
 import Delete from './components/Delete';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Add/>
       <Search/>
       <Delete/>
+      <ViewAll/>
 
 
     </div>
