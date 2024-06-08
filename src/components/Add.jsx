0f7b718fd9ab24d194f0b9df7d484c69e67f65af
Jafnak <jafnakusman@gmail.com>
2024-06-08 19:29:59 +0530

    Add component updated

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,8 +4,26 @@ const Add = () => {
   return (
     <div>
 
+    <div className="container">
+        <div className="row g-3">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">Id</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Title</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Price</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success">Add</button>
+            </div>
+        </div>
+    </div>
 
-        
     </div>
   )
 }
